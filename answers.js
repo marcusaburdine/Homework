d33ebbeae8affa77f8497d1e9744a97b206ead17
marcusaburdine <113939789+marcusaburdine@users.noreply.github.com>
2022-10-13 14:03:09 -0400

    Homework

---
+++ answers.js
@@ -0,0 +1,18 @@
+// Easy Going
+// Write a for loop that will log the numbers 1 through 20.
+
+
+for( let i = 0; i <= 20; i++){
+    console.log(i)
+}
+
+
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+// Hint: Think about the increment expression.
+
+for( let i = 0; i <= 20; i++){
+
+
+    console.log(i)
+}
