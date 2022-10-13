8dc3b1ec62bfbfe0bc8b24c2a9cd535ec14fe1a2
marcusaburdine <113939789+marcusaburdine@users.noreply.github.com>
2022-10-13 15:16:47 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -48,11 +48,11 @@
 // (name,species ,age, hometown)
 // You should be modifying the elements by accessing them. It is up to you which methods to use.
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
 // Plantee just had her birthday; change Plantee's array to reflect her being a year older.
 
@@ -74,3 +74,21 @@ const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
 // wolfy[0] = "Gameboy"
 // console.log(wolfy)
+
+
+// Yell at the Ninja Turtles
+// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+
+// const ninjaturtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+
+// for ( let turtles of ninjaturtles){
+
+//     console.log(turtles.toUpperCase())
+// }
+
+// Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+
+
+// As a developer, you'll be a lifelong learner and constantly encountering new things.
+// We'll give you little stretches like this to get you used to looking at documentation and figuring some things out on your own.
+// You've got this!
