b856316026a7cb0ded4f09c0e363c71949848e09
marcusaburdine <113939789+marcusaburdine@users.noreply.github.com>
2022-10-13 14:24:32 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -11,9 +11,33 @@
 
 // Hint: Think about the increment expression.
 
-for( let i = 0; i <= 200; i++){
+// for( let i = 0; i <= 200; i++){
 
-if (i % 2 === 0){
-    console.log(i)
-}
-}
+// if (i % 2 === 0){
+//     console.log(i)
+// }
+// }
+
+// Fizz Buzz
+// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+
+for( let i = 0; i <= 100; i++){
+
+    if (i % 3 === 0 && i % 5 === 0 ){
+        console.log(i + " FizzBuzz")
+    }
+
+    else if (i % 3 === 0){
+        console.log(i + " Fizz")
+    }
+
+    else if (i % 5 === 0){
+        console.log(i + " Buzz")
+    }
+    }
